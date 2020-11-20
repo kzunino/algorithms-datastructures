f02b7089892132dcb58b9e3564cd6e8e80ad142a
Kyle <kzunino@gmail.com>
2020-11-20 12:25:44 -0800

    added to URLify solution

--- ctci.chpt1/URLify.js
+++ ctci.chpt1/URLify.js
@@ -3,6 +3,9 @@ URLify a string soo as to replace all spaces in a string with %20.
 
 O(n) - because in worse case the replace method will traverse the whole method
 and not find a value to replace
+
+trim method only iterates over beginning and end of string until it runs out of 
+whitespace.
 */
 
 const URLify = (str) => {
