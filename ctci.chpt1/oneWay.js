83506fee359fcee3ad02cebff3a4acc728512e31
Kyle <kzunino@gmail.com>
2020-11-20 18:58:35 -0800

    added comments to one way

--- ctci.chpt1/oneWay.js
+++ ctci.chpt1/oneWay.js
@@ -24,6 +24,8 @@ Space Complexity: O(n) - construct an edited string
 const oneWay = (str1, str2) => {
   let result = false;
   // base cases:
+  // diff in len cant be more than one
+  // if strings are already the same then 0 edits required
   if (parseInt(str1.length - str2.length) > 1) return result;
   if (str1 === str2) return (result = true);
 
@@ -32,11 +34,14 @@ const oneWay = (str1, str2) => {
     let longerString = str1.length - str2.length === 1 ? str1 : str2;
     let shorterString = str1.length - str2.length === -1 ? str1 : str2;
 
+    // removes a character and compares edited string with shorter string
     for (let i = 0; i < str1.length; i++) {
       let editedString = longerString.replace(longerString[i], '');
       if (editedString === shorterString) return (result = true);
     }
+    // if strings are same length then we need to replace one character
   } else {
+    // replace character in str1 with complementary char in str2 and compare
     for (let i = 0; i < str1.length; i++) {
       let editedString;
       if (str1[i] !== str2[i]) editedString = str1.replace(str1[i], str2[i]);
