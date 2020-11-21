516f750f04178fb732e458d5067fe5bfead13120
Kyle <kzunino@gmail.com>
2020-11-21 11:17:45 -0800

    optmized pointer solution to keep track of edits and not brute force

--- ctci.chpt1/oneWay.js
+++ ctci.chpt1/oneWay.js
@@ -11,13 +11,17 @@ EXAMPLE
 Input: 'pale', 'ple'
 Output: True
 
-Input: Strings
+Input: String
 Output: Boolean
 Constraints: optimize
-Edge Case: empty string
+Edge Case: empty string, equal strings, 
 
 Time Complexity: O(n^2) - because we have to compare in order each character to another
 Space Complexity: O(n) - construct an edited string
+
+Optimized version:
+Time Complexity: O(n) - we only loop once and track the number of edits
+Space Complexity: O(1) - variables don't depend on the size of the string
 
 */
 
@@ -51,11 +55,52 @@ const oneWay = (str1, str2) => {
   return result;
 };
 
-console.log(oneWay('pale', 'ple') === true);
-console.log(oneWay('pales', 'pale') === true);
-console.log(oneWay('pale', 'bale') === true);
-console.log(oneWay('pale', 'pal') === true);
-console.log(oneWay('pale', 'bake') === false);
-console.log(oneWay('pales', 'bakeetf') === false);
-console.log(oneWay('lake', 'srake') === false);
-console.log(oneWay('lake', 'lake') === true);
+// console.log(oneWay('pale', 'ple') === true);
+// console.log(oneWay('pales', 'pale') === true);
+// console.log(oneWay('pale', 'bale') === true);
+// console.log(oneWay('pale', 'pal') === true);
+// console.log(oneWay('pale', 'bake') === false);
+// console.log(oneWay('pales', 'bakeetf') === false);
+// console.log(oneWay('lake', 'srake') === false);
+// console.log(oneWay('lake', 'lake') === true);
+
+// Optimized version:
+
+const oneWayOptimized = (str1, str2) => {
+  let edits = 1;
+  let maxLen = Math.max(str1.length, str2.length);
+  let difference = Math.abs(str1.length - str2.length);
+
+  // if strings diff is greater than 2 return false
+  if (difference > edits) {
+    return false;
+  }
+
+  for (let i = 0, j = 0; i < maxLen || j < maxLen; i++, j++) {
+    let str1Char = str1[i];
+    let str2Char = str2[j];
+    if (str1Char !== str2Char) {
+      edits--;
+      if (edits < 0) {
+        return false;
+      }
+      if (str1Char === str2[j + 1]) {
+        //inserted
+        j++;
+      } else if (str1[i + 1] === str2Char) {
+        //removed
+        i++;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(oneWayOptimized('pale', 'ple') === true);
+console.log(oneWayOptimized('pales', 'pale') === true);
+console.log(oneWayOptimized('pale', 'bale') === true);
+console.log(oneWayOptimized('pale', 'pal') === true);
+console.log(oneWayOptimized('pale', 'bake') === false);
+console.log(oneWayOptimized('pales', 'bakeetf') === false);
+console.log(oneWayOptimized('lake', 'srake') === false);
+console.log(oneWayOptimized('lake', 'lake') === true);
