bb0de1e47fdf6a6f7b24d0ccf5db7ccdfd3f845f
Kyle <kzunino@gmail.com>
2020-11-24 12:55:19 -0800

    solution for find Kth elem

---
+++ ctci.chpt2/returnKthToLast.js
@@ -0,0 +1,85 @@
+/* 
+CTCI - 2.1
+
+Return Kth to Last
+
+Implement an algorithm to find the kth to last element in a SSL
+
+EXAMPLE
+Input: Kth to last element
+Output: node
+
+Input: Kth to last element
+Output: node
+Constraints: optimize
+Edge Case: empty list or with one node
+
+Time Complexity: O(n) - we traverse the whole list if necessary
+Space Complexity: O(1)
+
+
+Optimized version:
+Time Complexity:
+Space Complexity:
+
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  toString() {
+    let head = this.head;
+    let result = [];
+    while (head) {
+      result.push(head.val);
+      head = head.next;
+    }
+    return result.join(', ');
+  }
+
+  findKthElement(k) {
+    if (k > this.length) return undefined;
+
+    let count = this.length - k;
+    let node = this.head;
+    while (count > 0) {
+      node = node.next;
+      count--;
+    }
+    return node;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push(3);
+list.push(2);
+list.push(3);
+list.push(1);
+list.push(4);
+// console.log(list);
+
+console.log(list.findKthElement(2));
+console.log(list.toString());
