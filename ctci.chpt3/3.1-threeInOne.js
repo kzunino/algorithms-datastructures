9bbde64e43ff4aa2927812e912dbf6f7f8844276
Kyle <kzunino@gmail.com>
2020-12-06 12:09:34 -0800

    solution for tripple stack - from internet =(

--- ctci.chpt3/3.1-threeInOne.js
+++ ctci.chpt3/3.1-threeInOne.js
@@ -1,6 +1,8 @@
 /* 
 CTCI - 3.1
 
+--solved from internet =( 
+
 Three In One
 
 Describe how you could use a single array to implement three stacks
@@ -14,8 +16,8 @@ Output:
 Constraints: 
 Edge Case: 
 
-Time Complexity: O(n) -
-Space Complexity: O(n) - 
+Time Complexity: O(1) - pop, peek, push
+Space Complexity: O(1) - push, pop, peek
 
 
 Optimized version:
@@ -23,3 +25,58 @@ Time Complexity:
 Space Complexity:
 
 */
+
+/**
+ * TripleStack class holds 3 stacks in one array. This is done by interleaving
+ * the values from the 3 indexes, so the first items are at 0, 1 and 2 and
+ * subsequent items are every 3 places from those. This class takes advantage
+ * of the fact that JavaScript arrays are dynamic and doesn't hold the stacks
+ * to any size. It doesn't reduce the size of the underlying array when items
+ * are popped but that could easily be added.
+ *
+ * Time: push O(1), pop O(1), peek O(1)
+ * Additional space: push O(1), pop O(1), peek O(1)
+ */
+
+class TrippleStack {
+  constructor() {
+    this.stack = [];
+    this.lengths = [0, 0, 0];
+  }
+
+  _getLength(stack) {
+    return this._lengths[stack - 1];
+  }
+
+  push(stack, value) {
+    let idx = this._getLength(stack) * 3 + stack - 1;
+    this._array[idx] = value;
+    ++this._lengths[stack - 1];
+  }
+
+  pop(stack) {
+    let length = this._getLength(stack),
+      value;
+    if (length > 0) {
+      let idx = (length - 1) * 3 + stack - 1;
+      value = this._array[idx];
+      this._array[idx] = undefined;
+      --this._lengths[stack - 1];
+    }
+    return value;
+  }
+
+  peek(stack) {
+    let length = this._getLength(stack),
+      value;
+    if (length > 0) {
+      let idx = (length - 1) * 3 + stack - 1;
+      value = this._array[idx];
+    }
+    return value;
+  }
+
+  isEmpty(stack) {
+    return this._getLength(stack) === 0;
+  }
+}
