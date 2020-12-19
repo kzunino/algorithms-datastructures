072e70c3fc436ca6bccd9edeead211a162c56f53
Kyle <kzunino@gmail.com>
2020-12-19 11:08:25 -0800

    project desc setup

---
+++ ctci.chpt4/4.2-minimalTree.js
@@ -0,0 +1,28 @@
+/* 
+CTCI - 4.2
+ 
+Minimal Tree
+
+Given a sorted (increasing order) array with unique integer elements,
+write an algorithm to create a binary search tree with minimal height.
+
+
+EXAMPLE
+Input: 
+Output: 
+
+Input:
+Output: 
+Constraints:  
+Edge Case:
+
+Time Complexity:
+
+Space Complexity: 
+
+
+Optimized version:
+Time Complexity:
+Space Complexity:
+
+*/
